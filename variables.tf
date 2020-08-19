5a6cc097a7315f5cdda8c285004e94ccff4efdd9
Pamela Garces <pamelag@codefabric.io>
2020-08-19 11:47:43 -0400

    udpate

--- variables.tf
+++ variables.tf
@@ -1,22 +1,27 @@
 variable domain_name {
   type        = string
+  description = "Domain name for website"
 }
 
 variable error_file {
   type        = string
   default     = "error.html"
+  description = "Reference the error html."
 }
 
 variable index_file {
   type        = string
   default     = "index.html"
+  description = "Reference the index html."
 }
 
 variable profile {
   type        = string
   default     = "default"
+  description = "AWS profile name"
 }
 
 variable region {
   type        = string
+  description = "AWS region"
 }
