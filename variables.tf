80d48ab4ae34e40ee5e204e4c2e92c45d61618cd
Pamela Garces <pamelag@codefabric.io>
2020-08-19 12:55:24 -0400

    update

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable domain_name {
   type        = string
-  description = "Domain name for website"
+  description = "Domain name for website."
 }
 
 variable profile {
   type        = string
   default     = "default"
-  description = "AWS profile name"
+  description = "AWS profile name."
 }
 
 variable region {
   type        = string
-  description = "AWS region"
+  description = "AWS region."
 }
